aa61509cfebaae8b5bcf3c3cb5dd691195f5fb28
vuckrishna <udaycv333@gmail.com>
2022-11-02 13:55:57 +0800

    hello

---
+++ testapp1/server.js
@@ -0,0 +1,23 @@
+var http = require("http"); 
+var fs = require('fs');
+var port = 3000;
+var serverUrl = "localhost";
+
+var server = http.createServer(function(req, res) {
+
+  console.log("Request: " + req.url);
+
+  if(req.url == "/index.html") {
+    
+        fs.readFile("index.html", function(err, text){
+          res.setHeader("Content-Type", "text/html");
+          res.end(text);
+        });
+        return;
+    
+   }
+
+});
+
+console.log("Listening at " + serverUrl + ":" + port);
+server.listen(port, serverUrl);
