8fcb588c97bf9de276a0cdfb7ababeb6bcb3ef00
udayviswa <udayviswa23@gmail.com>
2024-12-10 16:50:28 +0800

    small changes and new project upload

---
+++ UXPortfolio/script.js
@@ -0,0 +1,32 @@
+function introblock1(){
+    document.getElementById("block2").style.display = "none";
+    document.getElementById("block3").style.display = "none";
+    document.getElementById("block4").style.display = "none";
+    document.getElementById("block1").style.display = "block";
+    // document.getElementById("btn1").style.boxShadow = "0px 10px 10px darkgray";
+}
+function introblock2(){
+    document.getElementById("block1").style.display = "none";
+    document.getElementById("block3").style.display = "none";
+    document.getElementById("block4").style.display = "none";
+    document.getElementById("block2").style.display = "block";
+    // document.getElementById("btn2").style.boxShadow = "0px 10px 10px darkgray";
+}
+function introblock3(){
+    document.getElementById("block1").style.display = "none";
+    document.getElementById("block2").style.display = "none";
+    document.getElementById("block4").style.display = "none";
+    document.getElementById("block3").style.display = "block";
+    // document.getElementById("btn3").style.boxShadow = "0px 10px 10px darkgray";
+}
+function introblock4(){
+    document.getElementById("block1").style.display = "none";
+    document.getElementById("block2").style.display = "none";
+    document.getElementById("block3").style.display = "none";
+    document.getElementById("block4").style.display = "block";
+    // document.getElementById("btn4").style.boxShadow = "0px 10px 10px darkgray";
+}
+
+function alertfun(){
+    alert("This part is under development. Soon will be published, sorry for the inconvinence.");
+}
